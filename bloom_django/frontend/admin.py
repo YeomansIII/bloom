43068f51b6354e8b33a38ae630b92c4e431e1960
Minh <tranminh631@yahoo.com>
2015-09-05 14:46:12 -0400

    Added unicode definitions for objects

--- bloom_django/frontend/admin.py
+++ bloom_django/frontend/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from frontend.models import Player, PlantType, UserPlant, PlantImageZipFile
+from frontend.models import Player, PlantType, UserPlant, PlantImageZipFile, Timeline
 
 class UserPlantInline(admin.TabularInline):
     model = UserPlant
@@ -15,3 +15,4 @@ admin.site.register(Player, PlayerAdmin)
 admin.site.register(UserPlant)
 admin.site.register(PlantType)
 admin.site.register(PlantImageZipFile)
+admin.site.register(Timeline)
