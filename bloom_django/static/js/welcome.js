2e982e5bd6f67d50a73dac73cff5d09d1d9cf9f1
Miranda Wang <Miranda@Mirandas-MacBook-Pro.local>
2015-09-05 11:37:33 -0400

    Update welcome page, adding form method and action to sign up page.

--- bloom_django/static/js/welcome.js
+++ bloom_django/static/js/welcome.js
@@ -10,15 +10,27 @@ $(document).ready(function () {
         $(".plant-name").removeClass("plant-pick");
         $(".subtitle").replaceWith("<p class=\"subtitle\"> Give it a name: </p>");
         $(".plant-pick-container").replaceWith("<input class=\"plant-name-box\" type=\"text\" name=\"plant-name-box\"><input class=\"go\" type=\"submit\" value=\"Go!\" style=\"font-size:15px;padding:5px;\">");
+
+
+        $(".plant-name").on("submit", function(event){
+            console.log("TEST");
+            event.preventDefault();
+            $(".plant-name").addClass("bg-pick");
+            $(".bg-pick").removeClass("plant-name");
+            $(".subtitle").replaceWith("<p class=\"subtitle\"> Choose a background: </p>");
+            $(".plant-name-box").replaceWith("<div class=\"bg-pick-container\"><input type=\"image\" src=\"http://placehold.it/250x250\" alt=\"Background 1\"><input type=\"image\" src=\"http://placehold.it/250x250\" alt=\"Background 2\"><input type=\"image\" src=\"http://placehold.it/250x250\" alt=\"Background 3\"> </div>");
+
+        $(".bg-pick").on("submit", function(event){
+           $(".subtitle").replaceWith("<h2>You're all set! Ready to play? </h2>");
+            $(".bg-pick-container").remove();
+            $(".go").remove();
+        });
+
+        });
+
+
     });
 
-    $(".plant-name").on("submit", function(event){
-        event.preventDefault();
-        $(".plant-name").addClass("bg-pick");
-        $(".bg-pick").removeClass("plant-name");
-        $(".subtitle").replaceWith("<p class=\"subtitle\"> Choose a background: </p>");
-        $(".plant-name-box").replaceWith("<div class=\"bg-pick-container\"><input type=\"image\" src=\"http://placehold.it/250x250\" alt=\"Background 1\"><input type=\"image\" src=\"http://placehold.it/250x250\" alt=\"Background 2\"><input type=\"image\" src=\"http://placehold.it/250x250\" alt=\"Background 3\"> </div>");
-    });
 
 
 });
