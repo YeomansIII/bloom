d93d4dea021fdc85a75157bfdee9543b0c675ed3
Jason Yeomans <jyeo098@gmail.com>
2015-09-05 15:19:01 -0400

    create page changes

--- bloom_django/frontend/urls.py
+++ bloom_django/frontend/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
+    url(r'^welcome/$', views.welcome),
+    url(r'^create/$', views.create_plant),
     url(r'^play/$', views.play),
     url(r'^signup/$', views.create_user),
     url(r'^login/$', views.login_user),
