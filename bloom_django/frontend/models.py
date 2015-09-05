95b5d8bc5625150a7f354cda9c5b6ae85c44297e
Minh <tranminh631@yahoo.com>
2015-09-05 01:15:47 -0400

    Added user_plant

--- bloom_django/frontend/models.py
+++ bloom_django/frontend/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class User_Plant(models.Model):
+    name = models.CharField(max_length=100)
+    last_press = models.DateField(auto_now=False, auto_now_add=True)
+    created_date = models.DateField(auto_now=False, auto_now_add=True)
+    type = models.ForeignKey(Plant_Type)
+    owner = models.ForeignKey(Player)
