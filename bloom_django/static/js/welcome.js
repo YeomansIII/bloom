bb295393d46d34710609db47174e8b5cb3fc2574
Miranda Wang <Miranda@Mirandas-MacBook-Pro.local>
2015-09-05 11:12:47 -0400

    Creating welcome page.

---
+++ bloom_django/static/js/welcome.js
@@ -0,0 +1,27 @@
+/**
+ * Created by Miranda on 9/5/15.
+ */
+
+$(document).ready(function () {
+
+    $(".plant-pick").on("submit", function (event) {
+        event.preventDefault();
+        $(".plant-pick").addClass("plant-name");
+        $(".plant-name").removeClass("plant-pick");
+        $(".subtitle").replaceWith("<p class=\"subtitle\"> Give it a name: </p>");
+        $(".plant-pick-container").replaceWith("<input class=\"plant-name-box\" type=\"text\" name=\"plant-name-box\"><input class=\"go\" type=\"submit\" value=\"Go!\" style=\"font-size:15px;padding:5px;\">");
+    });
+
+    $(".plant-name").on("submit", function(event){
+        event.preventDefault();
+        $(".plant-name").addClass("bg-pick");
+        $(".bg-pick").removeClass("plant-name");
+        $(".subtitle").replaceWith("<p class=\"subtitle\"> Choose a background: </p>");
+        $(".plant-name-box").replaceWith("<div class=\"bg-pick-container\"><input type=\"image\" src=\"http://placehold.it/250x250\" alt=\"Background 1\"><input type=\"image\" src=\"http://placehold.it/250x250\" alt=\"Background 2\"><input type=\"image\" src=\"http://placehold.it/250x250\" alt=\"Background 3\"> </div>");
+    });
+
+
+});
+
+
+
