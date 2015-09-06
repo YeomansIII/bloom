9a12f79ef0b7faf1846fc93e4436e41a76a6f029
Minh <tranminh631@yahoo.com>
2015-09-06 05:35:17 -0400

    Added reward message for 3 consecutive days

--- bloom_django/static/js/play.js
+++ bloom_django/static/js/play.js
@@ -5,6 +5,14 @@
 $( document ).ready( function() {
         $('#game-menu-mobile').slicknav();
     $('.watering-can').on('click', function(){
+        var background = $(".flower").children()[0].src;
+        var dayIndex = background.lastIndexOf("Day");
+        var pngIndex = background.lastIndexOf(".png");
+        var picNo = Number(background.substring(dayIndex + 3, pngIndex));
+        if (picNo == 2) {
+            alert("3 Consecutive Days! Unlocked a new flower pot.");
+        }
+
         $.ajax({
             url: '/play/',
             dataType: 'text',
