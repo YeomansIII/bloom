2c0add04218420d2f3f8b43aeff931db6a3c3fdb
Miranda Wang <Miranda@Mirandas-MacBook-Pro.local>
2015-09-06 01:02:16 -0400

    Updated play JS to make ajax call on click of watering can.

--- bloom_django/static/js/play.js
+++ bloom_django/static/js/play.js
@@ -4,19 +4,22 @@
 
 $( document ).ready( function() {
         $('#game-menu-mobile').slicknav();
-    $.ajax({
-        url: '/play/',
-        dataType: 'text',
-        type: 'post',
-        success: function(data, status) {
-            if (data === 'error') {
-                alert("An error has occurred!");
-            } else {
-                /* Take new image and make that replace the old one plus some pretty animation. */
-                $('.flower').replaceWith('<div class="flower animate fadeIn"> <img src="' + data + '" </div>');
+    $('.watering-can').on('click', function(){
+        $.ajax({
+            url: '/play/',
+            dataType: 'text',
+            type: 'post',
+            success: function(data, status) {
+                if (data === 'error') {
+                    alert("An error has occurred!");
+                } else {
+                    /* Take new image and make that replace the old one plus some pretty animation. */
+                    $('.flower').replaceWith('<div class="flower animate fadeIn"> <img src="' + data + '" </div>');
+                }
             }
-        }
+        });
     });
+
     });
 
 
