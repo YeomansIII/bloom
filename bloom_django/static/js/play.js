e03398ca5cb7710fc649db8f3227675939a902fd
Miranda Wang <Miranda@Mirandas-MacBook-Pro.local>
2015-09-06 00:06:17 -0400

    Fixing conflicts.

--- bloom_django/static/js/play.js
+++ bloom_django/static/js/play.js
@@ -4,6 +4,19 @@
 
 $( document ).ready( function() {
         $('#game-menu-mobile').slicknav();
+    $.ajax({
+        url: '/play/',
+        dataType: 'text',
+        type: 'post',
+        success: function(data, status) {
+            if (data === 'error') {
+                alert("An error has occurred!");
+            } else {
+                /* Take new image and make that replace the old one plus some pretty animation. */
+                $('.flower').replaceWith('<div class="flower animate fadeIn"> <img src="' + data + '" </div>');
+            }
+        }
+    });
     });
 
 
