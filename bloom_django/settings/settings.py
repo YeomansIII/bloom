68b2fd4f7c823ea41a7893cafe3bc35cc9f74907
Jason Yeomans <jyeo098@gmail.com>
2015-09-09 09:04:33 -0400

    ignore static, css changes

--- bloom_django/settings/settings.py
+++ bloom_django/settings/settings.py
@@ -123,13 +123,13 @@ DATABASES = {
 }
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static/'),
 )
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static_root/")
 
 MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
